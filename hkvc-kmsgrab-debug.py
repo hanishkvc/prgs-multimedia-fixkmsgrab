3c8f4a48fed4eda1913a7954cafbf754e6b30900
hanishkvc <hanishkvc@gmail.com>
2020-06-24 00:04:00 +0530

    Rough idea of structure, python, pil, pyplot experiment
    As I am unable to get ffmpeg to pickup any DRM KMS related format
    modifiers, dug into python and its modules to try and explore the
    available image data and inturn infer how the data may be spreadout.
    
    Found that there seems to be a pattern every 128 pixels. So shaped
    the full image to be a very long 128 wide image.
    
    With this have also noticed that 8 adjacent rows, each of size 128
    pixels is grouped together.

--- hkvc-kmsgrab-debug.py
+++ hkvc-kmsgrab-debug.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
-# Experiment with ffmpeg KMSGrab
-# v20200623IST1750, HanishKVC
+# KMSGrab external format_modifier, as I am unable to get ffmpeg to do it.
+# v20200624IST0004, HanishKVC
 #
 
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL.Image
+
 
 CMD_CAPTURE="sudo ffmpeg -loglevel debug -f kmsgrab -device /dev/dri/card0 -format_modifier 0x0300000000000013 -i - -vf 'hwdownload,format=bgr0' -t 0.2 -f image2 test%03d.png"
 
 os.system(CMD_CAPTURE)
+
+
+p=PIL.Image.open("test001.png")
+t=p.getdata()
+a=np.array(p)
+a=np.array(t)
+b = a.sum(axis=1)/3
+1920*1080/128
+b10=b.reshape(16200,128)
+
+plt.matshow(b10[0:400,:])
+plt.show()
 
