ba7c3d2f28f6d3d879a2b333ccc3891f0f7cb5ae
hanishkvc <hanishkvc@gmail.com>
2020-06-23 20:15:00 +0530

    Try format_modifier
    Even thou I am able to capture the screen, the pixel data appears
    to be spread out across the overall screen area in some chunk size,
    like some kind of tiling being in place.
    
    Tried using the format_modifier option of kmsgrab using all possible
    intel fourcc_mod values from drm_fourcc.h, it didnt help.
    
    Even tried some format_modifiers related to nvidia, even that is not
    helping.
    
    Rather I dont think I am able to pass format_modifier to ffmpeg
    kmsgrab logic in a proper way, because, what ever the value I pass,
    the captured output remains the same.

--- hkvc-kmsgrab-debug.py
+++ hkvc-kmsgrab-debug.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-# Experiment with KMSGrab
-# v20200623IST1707, HanishKVC
+# Experiment with ffmpeg KMSGrab
+# v20200623IST1750, HanishKVC
 #
 
 import os
 
-os.system("sudo ffmpeg -loglevel debug -f kmsgrab -device /dev/dri/card0 -i - -vf 'hwdownload,format=bgr0' -t 0.2 -f image2 test%03d.png")
+CMD_CAPTURE="sudo ffmpeg -loglevel debug -f kmsgrab -device /dev/dri/card0 -format_modifier 0x0300000000000013 -i - -vf 'hwdownload,format=bgr0' -t 0.2 -f image2 test%03d.png"
+
+os.system(CMD_CAPTURE)
 
